e7d2f6f7c60b77e50ad3617a0e1e55cf99ea775e
Janiu <hyperjay55@gmail.com>
2020-08-07 19:36:10 +0100

    fixing the error bug that displays when an invalid movie is searched

--- app.js
+++ app.js
@@ -1,57 +1,3 @@
-<<<<<<< HEAD
-const express = require('express');
-const app = express();
-const axios = require('axios');
-const bodyParser = require('body-parser');
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static('public'));
-app.set('view engine', 'ejs');
-
-// INDEX
-app.get('/', function(req, res) {
-	res.render('index');
-});
-
-// FIND MOVIE ROUTE
-app.get('/movies', function(req, res) {
-	const query = req.query.search;
-	const url = `http://www.omdbapi.com/?apikey=thewdb&s=${query}`;
-	const getMovies = async (url) => {
-		try {
-			const response = await axios.get(url);
-			res.render('movies', { movies: response.data });
-		} catch (error) {
-			res.send(error.message);
-		}
-	};
-	getMovies(url);
-});
-
-// SHOW ROUTE
-app.get('/movies/:id', function(req, res) {
-	const query = req.params.id;
-	const getMovie = async () => {
-		try {
-			const response = await axios.get('http://www.omdbapi.com/', {
-				params: {
-					apikey: 'thewdb',
-					i: `${query}`,
-					plot: 'full'
-				}
-			});
-			res.render('show', { movie: response.data });
-		} catch (err) {
-			res.send(err.message);
-		}
-	};
-	getMovie();
-});
-
-const port = process.env.PORT || 3000;
-const ip = process.env.IP || '0.0.0.0';
-app.listen(port, ip);
-=======
 const express = require('express');
 const app = express();
 const axios = require('axios');
@@ -99,4 +45,3 @@ app.get('/movies/:id', function(req, res) {
 app.listen(3000, function() {
 	console.log('Movie App has started');
 });
->>>>>>> 75beaa211e1e79f38a4ce34e53b4390639821423
